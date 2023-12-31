16bee7f483aeb198150ffb61c822539d004097b0
Michael Lin <linmichael02@gmail.com>
2023-12-31 17:09:15 +0800

    Add API server start function and optimize route calculation

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,22 @@
+import os
+import datetime
+from base64 import b64decode
+from enum import Enum
+from sys import maxsize as INFINITY
+
 import flask
 from flask import request, jsonify
 import requests
 import json
-import os
 import argon2
 import pymongo
 import jwt
-import datetime
 import email_validator as eml_vldtr
 import ortools
-from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 import googlemaps
 import numpy as np
 
-from base64 import b64decode
 
 ENV_VAR_NAMES = ("GOOGLE_MAPS_API_KEY", "MONGO_HOSTNAME", "MONGO_USERNAME", "MONGO_PASSWORD", "FLASK_SECRET_KEY", "DEBUG")
 
@@ -48,6 +51,7 @@ TOKEN_VALIDITY = datetime.timedelta(hours=2)
 db = None
 
 def main():
+    """Starts the API server"""
     unset_vars = [name for name in ENV_VAR_NAMES if not os.environ.get(name)]
     if unset_vars:
         raise ValueError(f"Environment variables {*unset_vars,} not set")
@@ -77,7 +81,7 @@ def main():
             raise ValueError("DEBUG environment variable must be set to a boolean value")
     app.run(host="0.0.0.0", port=5000, debug=dbg_option)
 
-def token_auth(func):
+def token_auth(func: callable):
     """Decorator for token authentication"""
     def wrapper(*args, **kwargs):
         if not request.headers.get("Authorization"):
@@ -125,7 +129,7 @@ def login():
 
 @app.route("/api/register", methods=["POST"])
 def register():
-    """Attempts to register a username (valid email) and password pair."""
+    """Attempts to register a username (valid email) and password pair"""
     data = request.get_json()
     if not data:
         return jsonify({"error": "No data provided"}), 400
@@ -149,10 +153,58 @@ def register():
     })
     return jsonify({"error": None}), 200
 
+def simple_distance_matrix(client: googlemaps.Client, waypoints: list[str], transit_mode: TransitMode) -> list[list[int]]:
+    """Returns a simple distance (time) matrix for the given waypoints,
+    with each cell containing time in seconds.
+
+    Input:
+    - client: A Google Maps API client object
+    - waypoints: A list of Place IDs
+    - transit_mode: One of the transit mode constants
+    """
+    waypoints = ["place_id:"+place_id for place_id in waypoints]
+    response = client.distance_matrix(
+        waypoints,
+        waypoints,
+        mode=transit_mode.value,
+    )
+    # Extract the distance matrix (as values of time)
+    # (origin points)
+    rows = [row['elements'] for row in response['rows']]
+    # (origin, destination)
+    matrix = [[column['duration']['value'] for column in row] for row in rows]
+    return matrix
+
+def optimize_simple_matrix(matrix: list[list[int]]) -> dict:
+    """Optimizes a route based on the given distance matrix."""
+    manager = pywrapcp.RoutingIndexManager(len(waypoints), 1, 0)
+    routing = pywrapcp.RoutingModel(manager)
+    callback_index = routing.RegisterTransitCallback(
+        lambda i, j: matrix[i][j]
+    )
+    routing.SetArcCostEvaluatorOfAllVehicles(callback_index)
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters
+    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    solution = routing.SolveWithParameters(search_parameters)
+    if not solution:
+        raise RuntimeError("No solution found")
+    # Get order as list of indices for waypoints (a permutation)
+    optimized_order = []
+    index = routing.Start(0)
+    total_time = 0
+    while not routing.IsEnd(index):
+        prev = index
+        index = solution.Value(routing.NextVar(index))
+        total_time += routing.GetArcCostForVehicle(prev, index, 0)
+        optimized_order.append(manager.IndexToNode(index))
+    return {"order": optimized_order, "time": total_time}
+
 @app.route("/api/optimize")
 @token_auth
 def optimize():
-    """Optimizes a route based on the given parameters
+    """Optimizes a route based on the given parameters.
+
+    Input:
         - waypoints: A list of Place IDs
         If the mode is well-ordered, the first and last elements
         of the list are the start and end points respectively.
@@ -179,29 +231,39 @@ def optimize():
     if not isinstance(waypoints, list):
         return jsonify({"error": "Waypoints must be a list"}), 400
     client = googlemaps.Client(key=os.environ.get("GOOGLE_MAPS_API_KEY"))
+    matrix = simple_distance_matrix(client, waypoints, TransitMode[data["transit_mode"]])
     match data["tsp_mode"]:
         case TSPMode.VANILLA:
-            matrix = client.distance_matrix(
-                waypoints,
-                waypoints,
-                mode=data["transit_mode"],
-            )
-            # Create the router
-            router = pywrapcp.RoutingModel(
-                pywrapcp.RoutingIndexManager(
-                    len(waypoints),
-                    1,
-                    0,
-                )
-            )
-            # FIXME: Need to figure out what the return value of distance_matrix looks like
-            callback_index = router.RegisterTransitCallback(
-                lambda i, j: matrix[i][j]
-            )
+            try:
+                return jsonify(optimize_simple_matrix(matrix)), 200
+            except RuntimeError as e:
+                return jsonify({"error": e}), 400
         case TSPMode.START_CONSTRAINT:
-            pass
+            # Set all distances back to start as 0
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+            solution = None
+            try:
+                solution = optimize_simple_matrix(matrix)
+            except RuntimeError as e:
+                return jsonify({"error": e}), 400
+            solution['order'] = solution['order'][:-1]
+            return jsonify(solution)
         case TSPMode.START_END_CONSTRAINT:
-            pass
+            # Base Cases
+            # 1 waypoint
+            return jsonify({'order': [0], 'time': 0})
+            # TODO: Complete base cases
+
+            # Construct additional node v such that:
+            # - dist(nth node -> v) = 0
+            # - dist(v -> 0th node) = 0
+            # - dist(v -> all other nodes) = inf
+            # - dist(all other nodes -> v) = inf
+            # - dist(v -> v) = 0
+
+            # TODO: Complete construction of additional node            
+
         case TSPMode.SHORTEST_OVERALL:
             pass
         case _:
